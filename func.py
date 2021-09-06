9296a49bdeeef5e4f8073253c78922d74329bbf3
VMZ <vymiz020777@gmail.com>
2021-09-06 18:46:30 +0300

    win_check is done

--- func.py
+++ func.py
@@ -16,32 +16,59 @@ def play_field_draw(f):
             print(f[i][j], end=' ')
         print('\n')
 
-def smb_input():
+def smb_input(ctr, xo_arr):
     print('''
 выбери клетку, куда хочешь поставить свой значок
 сначала вводим номер строки, затем номер столбца''')
     r = input('Номер строки: ')
     c = input('Номер столбца: ')
     diapason = '012'
-    if (r in diapason) and (c in diapason):
+    if len(c) == 0 or len(r) == 0:
+        print('неверное значение')
+        return (False, False, ctr, xo_arr)
+    elif (r in diapason) and (c in diapason):
+        xo_arr.append(int(r+c))
+        xo_arr.sort()
         r = int(r)
         c = int(c)
     else:
         print('неверное значение')
-        return(False, False)
+        return(False, False, ctr, xo_arr)
 
-    return (r+1, c+1)
+    return (r+1, c+1, ctr-1, xo_arr)
 
-def rand_smb(f, xo_):
+def rand_smb(f, xo_, ctr, xo_arr_):
     smb_gen = True
     while smb_gen == True:
-        r = random.randint(1,3)
-        c = random.randint(1,3)
+        r_ = random.randint(1,3)
+        c_ = random.randint(1,3)
 
-        if f[r][c] != '-':
+        if f[r_][c_] != '-':
             continue
         else:
-            f[r][c] = xo_
+            f[r_][c_] = xo_
+            xo_arr_.append(int(str(r_-1)+str(c_-1)))
+            xo_arr_.sort()
             smb_gen = False
 
-    return (f)
+    return (f, r_, c_, ctr-1, xo_arr_)
+
+def win_check(f, win, XO_arr, smb, game, xo, xo_):
+
+    XO = set(XO_arr)
+
+    for i in win:
+        i = set(i)
+        if XO.issuperset(i):
+            game = False
+            print('Игра оrончена!')
+            if smb == xo:
+                print('ты виграл! Поздравляю! Возьми с полки пирожок!')
+                return(game)
+            elif smb == xo_:
+                print('Ты проиграл! Ничего, в следующий раз повезет!')
+                return(game)
+        else:
+            game = True
+
+    return(game)
--- main.py
+++ main.py
@@ -17,16 +17,29 @@ print(f'второй игрок будет играть симоволом {xo_}
 
 # создание пустого игрового поля и отрисовка его
 f = [[' ',0,1,2],[0,'-','-','-'],[1,'-','-','-'],[2,'-','-','-']] # an empty game field
+f_ctr = 9
 print('так выглядит наше игровое поле\n')
 func.play_field_draw(f)
 
 # основно цикл игры: получаем значок, проверям доступность клетки, проверяем условия для продолжения игры
 game = True
-r,c = False, False
+r,c, r_, c_ = False, False, None, None
+xo_arr = []
+xo_arr_ = []
+win = [
+    [0,1,2],
+    [10,11,12],
+    [20,21,22],
+    [0,10,20],
+    [1,11,21],
+    [2,12,22],
+    [0,11,22],
+    [20,11,2]
+    ]
 
 while game:
     # получение символа от игрока, проверка поля, размещение символа
-    r,c  = func.smb_input()
+    r,c, f_ctr, xo_arr  = func.smb_input(f_ctr, xo_arr)
 
     if not r or not c:
         continue
@@ -36,14 +49,33 @@ while game:
         continue
     else:
         f[r][c] = xo
-
+    print(xo_arr)
+    print(f_ctr)
     func.play_field_draw(f)
-    print('ход второго игрока\n')
-    time.sleep(2)
+    game = func.win_check(f, win, xo_arr, xo, game, xo, xo_)
+    if game == False:
+        break
 
     # получение символа для компа (второго игрока), проверка, размещение
-    f = func.rand_smb(f, xo_)
+    print('ход второго игрока\n')
+    time.sleep(1)
+    f, r_, c_, f_ctr, xo_arr_ = func.rand_smb(f, xo_, f_ctr, xo_arr_)
+
     func.play_field_draw(f)
+    print(xo_arr_)
+    print(f_ctr)
+    game = func.win_check(f, win, xo_arr_, xo_, game, xo, xo_)
+
+    if game == False:
+        break
+
+    if f_ctr ==0:
+        print("Свободных полей на поле больше нет. Ничья")
+        func.play_field_draw(f)
+        game = False
+        break
+
+
 
 
 
